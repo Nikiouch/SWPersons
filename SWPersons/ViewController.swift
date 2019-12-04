06f858298c61abeedf8d69c68b721b8cc9b5d11d
Nikiouch <hanouchh@gmail.com>
2019-12-04 20:24:24 +0300

    Initial Commit

---
+++ SWPersons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SWPersons
+//
+//  Created by Никита Главацкий on 04.12.2019.
+//  Copyright © 2019 StreetPeople. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
